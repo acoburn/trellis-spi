65107e1673330a2c03163055942d4a3b9a4d6ea4
Aaron Coburn <acoburn@amherst.edu>
2016-12-31 16:08:25 -0500

    add a serialization service

---
+++ src/main/java/edu/amherst/acdc/trellis/spi/SerializationService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.trellis.spi;
+
+import java.io.OutputStream;
+import java.util.stream.Stream;
+
+import org.apache.commons.rdf.api.Triple;
+
+/**
+ * @author acoburn
+ */
+public interface SerializationService {
+
+    /**
+     * Serialize the triple stream as RDF
+     * @param triples the stream of triples
+     * @param output the output stream
+     * @param format the output format
+     */
+    void serialize(final Stream<Triple> triples, final OutputStream output, final String format);
+
+    /**
+     * Serialize the triple stream as RDF
+     * @param triples the stream of triples
+     * @param output the output stream
+     * @param format the output format
+     * @param profile additional profile information used for output
+     */
+    void serialize(final Stream<Triple> triples, final OutputStream output, final String format, final String profile);
+
+}
