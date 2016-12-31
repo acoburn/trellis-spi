614a52a99d0754fe6d1c533c60b312a821122c6e
Aaron Coburn <acoburn@amherst.edu>
2016-12-31 15:31:04 -0500

    add fixity service

--- src/main/java/edu/amherst/acdc/trellis/spi/ResourceService.java
+++ src/main/java/edu/amherst/acdc/trellis/spi/ResourceService.java
@@ -18,6 +18,7 @@ package edu.amherst.acdc.trellis.spi;
 import java.util.Optional;
 
 import edu.amherst.acdc.trellis.api.Resource;
+import edu.amherst.acdc.trellis.api.Session;
 import org.apache.commons.rdf.api.IRI;
 
 /**
@@ -27,32 +28,36 @@ public interface ResourceService {
 
     /**
      * Find a resource at the given location
+     * @param session the session
      * @param identifier the resource identifier
      * @return the resource
      */
-    Optional<Resource> find(final IRI identifier);
+    Optional<Resource> find(final Session session, final IRI identifier);
 
     /**
      * Find a resource at the given location
+     * @param session the session
      * @param identifier the resource identifier
      * @param type the resource type
      * @return the resource
      */
-    <T> Optional<T> find(final IRI identifier, final Class<T> type);
+    <T> Optional<T> find(final Session session, final IRI identifier, final Class<T> type);
 
     /**
      * Test whether a resource exists at the identifier
+     * @param session the session
      * @param identifier the resource identifier
      * @return whether the identified resource exists
      */
-    Boolean exists(final IRI identifier);
+    Boolean exists(final Session session, final IRI identifier);
 
     /**
      * Create a new resource
+     * @param session the session
      * @param identifier the Identifier for the new resource
      * @param type the type of the resource
      * @param <T> the resource type
      * @return the new resource
      */
-    <T> Optional<T> create(final IRI identifier, final Class<T> type);
+    <T> Optional<T> create(final Session session, final IRI identifier, final Class<T> type);
 }
