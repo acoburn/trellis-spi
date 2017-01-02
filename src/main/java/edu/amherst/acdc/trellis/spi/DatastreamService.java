f55cb822a2d7fb2d1cd5713ba5a41e2d92a1782a
Aaron Coburn <acoburn@amherst.edu>
2017-01-01 20:04:26 -0500

    add datastream service

---
+++ src/main/java/edu/amherst/acdc/trellis/spi/DatastreamService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.trellis.spi;
+
+import java.io.InputStream;
+
+import org.apache.commons.rdf.api.IRI;
+
+/**
+ * @author acoburn
+ */
+public interface DatastreamService {
+
+    /**
+     * Get the content of the datastream
+     * @param identifier an identifier used for locating the datastream
+     * @return the content
+     */
+    InputStream getContent(final IRI identifier);
+
+    /**
+     * Test whether a datastream exists at the given URI
+     * @param identifier the datastream identifier
+     * @return whether the datastream exists
+     */
+    Boolean exists(final IRI identifier);
+
+    /**
+     * Set the content for a datastream
+     * @param identifier the identifier
+     * @param stream the content
+     * @param contentType the contentType
+     */
+    void setContent(final IRI identifier, final InputStream stream, final String contentType);
+
+}
