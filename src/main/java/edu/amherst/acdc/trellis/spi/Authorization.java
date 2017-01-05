e4a5db3e6c0efb2b00522c26b65ae057a88b7437
Aaron Coburn <acoburn@amherst.edu>
2017-01-05 09:25:02 -0500

    change Authorization interface to a class

--- src/main/java/edu/amherst/acdc/trellis/spi/Authorization.java
+++ src/main/java/edu/amherst/acdc/trellis/spi/Authorization.java
@@ -15,12 +15,20 @@
  */
 package edu.amherst.acdc.trellis.spi;
 
-import java.util.Collection;
+import static java.util.Collections.unmodifiableSet;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import edu.amherst.acdc.trellis.vocabulary.ACL;
+
+import org.apache.commons.rdf.api.Graph;
 import org.apache.commons.rdf.api.IRI;
 
 /**
- * This interface provides access to data defined in an WebAC Authorization graph. Access to a resource can be
+ * This class provides access to data defined in an WebAC Authorization graph. Access to a resource can be
  * controlled via WebAccessControl, an RDF-based access control system. A resource can define an ACL resource
  * via the Link header, using rel=acl. It can also point to an ACL resource using a triple in the resource's own
  * RDF graph via acl:accessControl. Absent an acl:accessControl triple, the parent resource is checked, up to the
@@ -34,53 +42,96 @@ import org.apache.commons.rdf.api.IRI;
  *
  * @author acoburn
  */
-public interface Authorization {
+public class Authorization {
+
+    private final IRI identifier;
+    private final Map<IRI, Set<IRI>> dataMap = new HashMap<>();
+
+
+    /**
+     * Create an Authorization object from an RDF graph
+     * @param identifier the subject IRI
+     * @param graph the RDF graph
+     */
+    public Authorization(final IRI identifier, final Graph graph) {
+        this.identifier = identifier;
+        dataMap.put(ACL.agent, new HashSet<>());
+        dataMap.put(ACL.agentClass, new HashSet<>());
+        dataMap.put(ACL.agentGroup, new HashSet<>());
+        dataMap.put(ACL.mode, new HashSet<>());
+        dataMap.put(ACL.accessTo, new HashSet<>());
+        dataMap.put(ACL.accessToClass, new HashSet<>());
+        dataMap.put(ACL.defaultForNew, new HashSet<>());
+
+        graph.stream(identifier, null, null).forEach(triple -> {
+            if (dataMap.containsKey(triple.getPredicate()) && triple.getObject() instanceof IRI) {
+                dataMap.get(triple.getPredicate()).add((IRI) triple.getObject());
+            }
+        });
+    }
 
     /**
      * Retrieve the identifier for this Authorization
      * @return the identifier
      */
-    IRI getIdentifier();
+    public IRI getIdentifier() {
+        return identifier;
+    }
 
     /**
      * Retrieve the agents that are associated with this Authorization
      * @return the Agent values
      */
-    Collection<String> getAgents();
+    public Set<IRI> getAgent() {
+        return unmodifiableSet(dataMap.get(ACL.agent));
+    }
 
     /**
      * Retrieve the agent classes that are associated with this Authorization
      * @return the Agent class values
      */
-    Collection<IRI> getAgentClasses();
+    public Set<IRI> getAgentClass() {
+        return unmodifiableSet(dataMap.get(ACL.agentClass));
+    }
 
     /**
      * Retrieve the agent groups that are associated with this Authorization
      * @return the Agent groups values
      */
-    Collection<IRI> getAgentGroups();
+    public Set<IRI> getAgentGroup() {
+        return unmodifiableSet(dataMap.get(ACL.agentGroup));
+    }
 
     /**
      * Retrieve the access modes that are associated with this Authorization
      * @return the access mode values
      */
-    Collection<IRI> getModes();
+    public Set<IRI> getMode() {
+        return unmodifiableSet(dataMap.get(ACL.mode));
+    }
 
     /**
      * Retrieve the resource identifiers to which this Authorization applies
      * @return the accessTo values
      */
-    Collection<IRI> getAccessTo();
+    public Set<IRI> getAccessTo() {
+        return unmodifiableSet(dataMap.get(ACL.accessTo));
+    }
+
 
     /**
      * Retrieve the accessToClass values that are associated with this Authorization
      * @return the accessToClass values
      */
-    Collection<IRI> getAccessToClasses();
+    public Set<IRI> getAccessToClass() {
+        return unmodifiableSet(dataMap.get(ACL.accessToClass));
+    }
 
     /**
      * Retrieve the directories for which this authorization is used for new resources in the container
      * @return the resource identifiers
      */
-    Collection<IRI> getDefaultForNew();
+    public Set<IRI> getDefaultForNew() {
+        return unmodifiableSet(dataMap.get(ACL.defaultForNew));
+    }
 }
