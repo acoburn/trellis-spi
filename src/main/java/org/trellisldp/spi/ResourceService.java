e67e46512c219720590f197241f930c522429e04
Aaron Coburn <acoburn@amherst.edu>
2017-08-23 22:05:48 -0400

    Add internal/external function

--- src/main/java/org/trellisldp/spi/ResourceService.java
+++ src/main/java/org/trellisldp/spi/ResourceService.java
@@ -33,6 +33,8 @@ import org.trellisldp.api.Resource;
  */
 public interface ResourceService {
 
+    public static final String TRELLIS_PREFIX = "trellis:";
+
     /**
      * Get a resource from the given location
      * @param identifier the resource identifier
@@ -99,6 +101,24 @@ public interface ResourceService {
      * @return a blank node, if a previously-skolemized node; otherwise the original term
      */
     RDFTerm unskolemize(RDFTerm term);
+
+    /**
+     * Convert an internal term to an external term
+     * @param <T> the RDF term type
+     * @param term the RDF term
+     * @param baseUrl the base URL
+     * @return a converted RDF term
+     */
+    <T extends RDFTerm> T toExternalTerm(T term, String baseUrl);
+
+    /**
+     * Convert an external term to an internal term
+     * @param <T> the RDF term type
+     * @param term the RDF term
+     * @param baseUrl the base URL
+     * @return a converted RDF term
+     */
+    <T extends RDFTerm> T toInternalTerm(T term, String baseUrl);
 
     /**
      * Export the complete repository as a stream of Quads
